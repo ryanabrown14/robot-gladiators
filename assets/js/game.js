a488d368c6789e12bdfbe95ce2abd579ea88d505
ry brown <ryan.alex.brown@gmail.com>
2020-08-11 23:45:31 -0700

    MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,9 +67,43 @@ var fight = function(enemyName) {
       }
     }
   };
-
+var startGame = function(){
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 for(var i = 0; i <enemyNames.length; i++){
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!")
+        break;
+    }
+
 }
+endGame();
+};
+//function to end the entire game
+var endGame = function(){
+    //if the player is still alive, the player wins
+    if (playerHealth > 0) {
+        window.alert("Great Job, you're survived the game! You now have a score of " + playerMoney + ".");
+
+    }
+    else{
+        window.alert("You've lost your robot in battle.")
+    }
+    var playAgainConfirm = window.confirm ("Would you like to play again?");
+    if (playAgainConfirm){
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! come back soon!");
+    }
+
+};
+startGame();
