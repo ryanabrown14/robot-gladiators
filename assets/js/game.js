f765accbba5df92252306e4fa94fc9abc9c67d3e
ry brown <ryan.alex.brown@gmail.com>
2020-08-15 17:21:18 -0700

    player name null

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,6 +66,7 @@ playerInfo.reset();
 for(var i = 0; i <enemyInfo.length; i++){
     if (playerInfo.health > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+        
         var pickedEnemyObj = enemyInfo[i];
         pickedEnemyObj.health = randomNumber(40, 60);
         fight(pickedEnemyObj);
@@ -145,8 +146,17 @@ var shop = function() {
 
   }
 };
+var getPlayerName = function (){
+  var name = "";
+  while (name === "" || name === null){
+    name = prompt("What is your robot's name?")
+  }
+  console.log("Your robot's name is" + name);
+  return name;
+
+};
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
